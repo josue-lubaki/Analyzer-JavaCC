9f0245c8de32c9ec15224472f3b7aa1556e9698e
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-12-03 02:22:44 -0500

    init project

---
+++ src/Application/User.java
@@ -0,0 +1,14 @@
+package Application;
+
+public class User {
+	private String fname;
+	private String lname;
+	private int age;
+	
+	public User(String fname, String lname, int age) {
+		this.fname = fname;
+		this.lname = lname;
+		this.age = age;
+	}
+	
+}
